5d3b3c8546d52fb0dd1444fd5ccd915156715ffe
kattaGowthami <85305250+kattaGowthami@users.noreply.github.com>
2023-03-07 22:24:04 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.5.5-10.1.13-MariaDB : Database - bone
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bone` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `bone`;
+
+/*Table structure for table `ureg` */
+
+DROP TABLE IF EXISTS `ureg`;
+
+CREATE TABLE `ureg` (
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `pwd` varchar(100) DEFAULT NULL,
+  `cpwd` varchar(100) DEFAULT NULL,
+  `addr` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ureg` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
